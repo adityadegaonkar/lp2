fdd73daf1cd2e629e1c442cba1c6298299efd52a
adityadegaonkar <degaonkaraditya10@gmail.com>
2024-05-09 13:20:40 +0530

    Update python1.py

--- python1.py
+++ python1.py
@@ -2,9 +2,13 @@ a = int(input("Enter the number")
 b = int(input("Enter he number")
 
 def max(a,b):
-if a>b:
+  if a>b:
 print("a is greater")
-else:
-print("b is greater)
+  else:
+print("b is greater")
+
+print(max(a,b))
+
+  
 
 
